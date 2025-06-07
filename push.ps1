cd4cacc80f049a63fecb0a1ce85f00350adf7acd
JACK MAC <donkeycon@protonmail.com>
2025-06-08 02:45:26 +1000

    auto

--- push.ps1
+++ push.ps1
@@ -1,8 +1,8 @@
-python3.9 mysite/manage.py collectstatic --noinput --clear
+python mysite/manage.py collectstatic --noinput --clear
 sleep 1
-python3.9 mysite/manage.py makemigrations
+python mysite/manage.py makemigrations
 sleep 1
-python3.9 mysite/manage.py migrate
+python mysite/manage.py migrate
 sleep 1
 mv mysite/mysite/settings.py mysite/mysite/localsettings.py
 sleep 1
@@ -12,13 +12,13 @@ git add .
 sleep 1
 git commit -m "auto"
 sleep 1
-git push --set-upstream origin main --force
+git push
 sleep 1
-git push heroku main:main --force
+git push heroku
 sleep 1
 mv mysite/mysite/settings.py mysite/mysite/herokusettings.py
 sleep 1
 mv mysite/mysite/localsettings.py mysite/mysite/settings.py
 sleep 1
-heroku run python mysite/manage.py migrate
+heroku run python3 mysite/manage.py migrate
 sleep 1
